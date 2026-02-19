0d39caa305c18ceff5e31b829541e76718c49f87
AngelRosadoWTF <angsoft05@gmail.com>
2026-02-19 16:07:17 -0600

    Cambios
    Cambios.

--- config/routes.php
+++ config/routes.php
@@ -17,8 +17,3 @@ $router->get('/', fn() => [
 
 $router->get('/api/productos', fn() => $controller->getProductos());
 $router->get('/api/stock-productos', fn() => $controller->getStockProductos());
-$router->get('/api/categorias', fn() => $controller->getCategorias());
-$router->get('/api/categorias2', fn() => $controller->getCategorias2());
-$router->get('/api/ordenes', fn() => $controller->getOrdenes());
-
-
